4d246a385e73b2acb9fd85cdc856ac1c4f11620f
kldrozdz <61559912+kldrozdz@users.noreply.github.com>
2020-02-27 19:59:50 +0100

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace _2BLab1
+{
+    class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers";
+            var command = new SqlCommand(query, connection);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+
+            reader.Close();
+        }
+
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionId, regionDescription)" + "VALUES (@id, @description)";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id",id);
+            command.Parameters.AddWithValue("@description",description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+
+        public void Delete(SqlConnection connection, int id, string description)
+        {
+            var query = "DELETE FROM region WHERE regionId = @id AND regionDescription = @description";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description); // dodawanie parametrow do komendy
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _2BLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DB();
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB...;";
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 10, "Bielsko");
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
